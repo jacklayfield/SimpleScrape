80140a4b05c3735f4985d5acbd97c07c07227dda
jacklayfield <layf100@gmail.com>
2022-07-19 23:29:21 -0400

    Basic functionality for scraping a single site, better filtering

--- api/scraper.js
+++ api/scraper.js
@@ -1,3 +1,27 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+const { filterEmails } = require("./utils");
+
+request(
+  "https://www.nutstop.com/product-category/nuts-seeds/peanuts/",
+  (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+
+      var tags = [];
+
+      $("a").each((_, e) => {
+        let row = $(e).text().replace(/(\s+)/g, " ");
+        console.log(`${row}`);
+        tags.push(`${row}`);
+      });
+    }
+
+    console.log(filterEmails(tags));
+  }
+);
+
 //Will include main function calls
 
 //Helper functions here
--- api/utils.js
+++ api/utils.js
@@ -12,6 +12,9 @@ const stripChars = (num) => num.replace(/\D/g, "");
 /* takes in an array and filters out null values */
 const filterNulls = (arr) => arr.filter((elem) => elem !== null);
 
-//Request and Response
+/* takes in an array and filters out all elems without "@" */
+const filterEmails = (arr) => arr.filter((elem) => elem.includes("@"));
 
 //DOM parsing
+
+module.exports = { removeHttps, stripChars, filterNulls, filterEmails };
