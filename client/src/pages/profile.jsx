5bed87ecaf146a3dadf62c257e648754e53f5c2b
jacklayfield <layf100@gmail.com>
2022-09-08 16:11:06 -0400

    Adding mobile formatting to profile page

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "../styling/about.css";
@@ -28,7 +28,47 @@ export function Profile() {
   const q2 = cards.slice(quarter, quarter * 2);
   const q3 = cards.slice(quarter * 2, quarter * 3);
   const q4 = cards.slice(quarter * 3, cards.length);
-  return (
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const breakpoint = 620;
+
+  React.useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+
+    // Return a function from the effect that removes the event listener
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
+  return width < breakpoint ? (
+    <div>
+      <Row className="gx-0">
+        <Col>
+          <div className="columns">
+            <div className="sectionTitles">
+              <header className="sectionTitlesText">My Scrape Results</header>
+            </div>
+            <div style={{ paddingLeft: 20, paddingRight: 20, paddingTop: 20 }}>
+              <Row>
+                <Col>
+                  <ScrapeCards scrapeCards={cards}></ScrapeCards>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="gx-0">
+        <Col>
+          <div className="columns">
+            <div className="sectionTitles">
+              <header className="sectionTitlesText">My Information</header>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  ) : (
     <div>
       <Row className="gx-0">
         <Col>
@@ -67,4 +107,7 @@ export function Profile() {
       </Row>
     </div>
   );
+  // return (
+
+  // );
 }
