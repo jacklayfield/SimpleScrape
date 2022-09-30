29237e323bc61a30365a83bb8b35cc7ceac85412
jacklayfield <layf100@gmail.com>
2022-09-30 18:29:24 -0400

    Devising method to grab valid emails and phone numbers from data

--- api/utils.js
+++ api/utils.js
@@ -21,6 +21,9 @@ const filterEmails = (arr) => arr.filter((elem) => elem.includes("@"));
 /* takes in an array and filters out all elems without "www." */
 const filterUrls = (arr) => arr.filter((elem) => elem.includes("www."));
 
+/* takes in an array and filters out all elems with a length less than a normal phone # (10) */
+const filterPhone = (arr) => arr.filter((elem) => elem.length >= 10);
+
 /* takes in an array with strings containing urls and isolates them (useful for engines that don't make scraping easy)*/
 function sanitizeUrl(arr) {
   tmpVals = [];
@@ -39,6 +42,38 @@ function sanitizeUrl(arr) {
 
   return retVals;
 }
+
+function filterData() {
+  var theArr = [
+    "13048",
+    "guy@ex.com",
+    "gal@ex.com",
+    "dogs",
+    "1029394891",
+    "bob",
+    "59@b.com",
+  ];
+  var emailArr = filterEmails(theArr);
+
+  phoneArr = [];
+
+  theArr.forEach((elem) => {
+    // var newElem = elem.stripChars();
+    var newElem = stripChars(elem);
+    phoneArr.push(newElem);
+  });
+
+  // var newArr1 = stripChars(newArr);
+  var phoneArrNum = filterBlanks(phoneArr);
+
+  phoneArrNumFiltered = filterPhone(phoneArrNum);
+
+  const resArray = emailArr.concat(phoneArrNumFiltered);
+
+  console.log(resArray);
+}
+
+filterData();
 
 // array1 = ["eiosfj esiojf seof gghedwww.test.org grhse", "wwww.hello.net wfwef"];
 
