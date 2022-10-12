304ab49158ee21488d4ca96e5c63e4f9db7e839e
jacklayfield <layf100@gmail.com>
2022-10-11 23:44:56 -0400

    Adding new single scrape page, preparing for scrape card changes

---
+++ client/src/pages/singleScrape.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "../styling/about.css";
+
+export function singleScrape() {
+  return <div></div>;
+}
